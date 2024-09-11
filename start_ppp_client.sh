49c246546ccd36a09b82bd23bea94fe2697a0962
aviral.verma <aviral.verma.8877@gmail.com>
2024-09-11 12:28:52 +0530

    update

--- start_ppp_client.sh
+++ start_ppp_client.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# esptool.py --after hard_reset chip_id
-# sleep 1
-# sudo stty -F /dev/ttyUSB0 raw
+esptool.py --after hard_reset chip_id
+sleep 1
+sudo stty -F /dev/ttyUSB0 raw
 sudo pppd /dev/ttyUSB0 115200 10.0.5.1:10.0.5.2 proxyarp local noauth debug nodetach dump nocrtscts passive persist maxfail 0 holdoff 1 mtu 250 mru 250
 
