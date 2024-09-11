0ae404a7668d22e367d14b55772f4450fae29b86
aviral.verma <aviral.verma.8877@gmail.com>
2024-09-11 12:20:40 +0530

    update

--- start_ppp_server.sh
+++ start_ppp_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-esptool.py --after hard_reset chip_id
-sleep 1
+# esptool.py --after hard_reset chip_id
+# sleep 1
 # sudo stty -F /dev/ttyUSB0 raw
 # sudo stty -F /dev/ttyUSB0 -a
-sudo pppd /dev/ttyUSB0 115200 10.0.5.2:10.0.5.1 noauth local debug dump defaultroute nocrtscts persist maxfail 0 holdoff 1 mtu 128 mru 128
+sudo pppd /dev/ttyUSB0 115200 10.0.5.2:10.0.5.1 noauth local debug dump defaultroute nocrtscts persist maxfail 0 holdoff 1 mtu 250 mru 250
